364f6d51e491280050fe97233305a1d04343eb82
Mohammad Ali Toufighi <alitoumob@gmail.com>
2018-12-14 18:25:18 +0330

    add equals method for type

--- src/main/ast/Type/Type.java
+++ src/main/ast/Type/Type.java
@@ -2,4 +2,8 @@ package ast.Type;
 
 public abstract class Type {
     public abstract String toString();
+
+    public boolean equals(Type obj) {
+        return this.toString().equals(obj.toString());
+    }
 }
